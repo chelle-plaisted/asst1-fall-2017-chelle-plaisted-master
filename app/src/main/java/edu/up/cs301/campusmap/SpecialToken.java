c4a9c908b07ab96bdf955ee9202b79c5ea548cd0
chelle-plaisted <plaisted20@up.edu>
2017-09-02 11:17:21 -0700

    SqaureToken class ready

---
+++ app/src/main/java/edu/up/cs301/campusmap/SpecialToken.java
@@ -0,0 +1,8 @@
+package edu.up.cs301.campusmap;
+
+/**
+ * Created by Rachel on 9/2/2017.
+ */
+
+public class SpecialToken extends SimpleMapToken {
+}
---
+++ app/src/main/java/edu/up/cs301/campusmap/SquareToken.java
@@ -0,0 +1,36 @@
+package edu.up.cs301.campusmap;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+
+/**
+ * <!-- class SimpleMapToken -->
+ *
+ * This class represents a single map token that can be drawn on a canvas
+ *
+ * @author
+ * @version September 2017
+ */
+public class SquareToken extends SimpleMapToken {
+
+
+    /**
+     * Constructor
+     * @param xCoord the x-coordinate
+     * @param yCoord the y-coordinate
+     */
+    public SquareToken(float xCoord, float yCoord) {
+        super(xCoord, yCoord);
+    }//constructor
+
+    /**
+     * this token can paint itself on a given canvas
+     * @param canvas the canvas on which to draw
+     */
+    public void drawOn(Canvas canvas) {
+        // paint the dot on the canvas
+        canvas.drawRect(x,y, x+ radius * 2, y + radius * 2, paint);
+    }//drawOn
+
+}
